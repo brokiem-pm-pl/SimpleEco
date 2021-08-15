c4de4d25b93845116c23679f0a8b762a9fa4f185
brokiem <milyhqvideo@gmail.com>
2021-08-15 11:14:37 +0800

    add files

---
+++ src/brokiem/simpleco/SimpleEco.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace brokiem\simpleco;
+
+use pocketmine\plugin\PluginBase;
+
+class SimpleEco extends PluginBase {
+
+    protected function onEnable(): void {
+
+    }
+}
