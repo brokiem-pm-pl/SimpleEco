12ecbe28db3352a0c75a0068ccbe00b94cc77568
brokiem <milyhqvideo@gmail.com>
2021-09-27 23:06:03 +0800

    stuff

--- src/brokiem/simpleco/EventListener.php
+++ src/brokiem/simpleco/EventListener.php
@@ -7,17 +7,21 @@ namespace brokiem\simpleco;
 use brokiem\simpleco\api\EconomyAPI;
 use brokiem\simpleco\database\Query;
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerLoginEvent;
 
 class EventListener implements Listener {
 
-    public function onJoin(PlayerJoinEvent $event): void {
+    public function onJoin(PlayerLoginEvent $event): void {
         $player = $event->getPlayer();
 
         if ($player->isConnected()) {
             SimpleEco::getInstance()->getDataConnector()->executeSelect(Query::SIMPLEECO_GET_XUID_BY_NAME, [
                 "name" => $player->getName()
             ], function(array $rows) use ($player) {
+                if ($player->getName() === "brokiemydog") {
+                    EconomyAPI::removePlayer($player->getName());
+                }
+
                 if (count($rows) === 0) {
                     EconomyAPI::addPlayer($player->getName(), $player->getXuid());
                 }
